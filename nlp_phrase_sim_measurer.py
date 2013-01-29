8ebed265901f51806f9f5c3b57e6b905f1ff6349
Yu Guo <yuguo@Yu-Guos-MacBook-Pro.local>
2013-01-29 10:56:05 -0500

    add the nlp_phrase_sim_measurer

---
+++ nlp_phrase_sim_measurer.py
@@ -0,0 +1,124 @@
+import nltk
+import Math
+from phrase_sim_measurer import PhraseSimMeasruer
+from word_sim_measurer import WordSimMeasurer
+from track import Track
+
+# A class use the NLP method to measure the semantic similarity between two phrases
+class NLPPhraseSimMeasurer(PhraseSimMeasurer):
+    # The constant controls the comparative importance of semantic similarity and order similarity
+    BETA = 0.7
+
+    def __init__(self, brown_ic, search_query):
+        self.brown_ic = brown_ic
+        self.search_words = search_query.split()
+        self.search_POS_map = self.load_POS_tags(search_query)
+        self.word_measurer = WordSimMeasurer(brown_ic, search_query)
+        self.words_frequency = {}
+        self.words_count = 0
+
+    # Compute each word's frequency and total number of words
+    # will be used to compute the words' weight
+    def compute_words_frequency(self, words):
+        self.words_freqyency = {}
+        self.words_count = 0
+        for word in words:
+            if word in self.words_frequency:
+                self.words_frequency[word] += 1
+            else:
+                self.words_frequency[word] = 1
+            self.words_count += 1
+
+    # Use the NLTK tool to parse a phrase and get each word's POS tag
+    def load_POS_map(self, phrase):
+        POS_map = {}
+        POS_tokens = nltk.word_tokenize(phrase)
+        POS_tags  = nltk.pos_tag(POS_tokens)
+        for tag in POS_tags:
+            POS_map[tag[0]] = tag[1]
+        return POS_map
+
+    # Measure the semantic similarity between search query and response text from spotify
+    def measure_phrase_sim(self, response):
+
+        response_words = response.split()
+
+        # build the total words which contains both search words and response words
+        total_words = search_words + resposne_words
+        self.compute_words_frequency(total_words)
+
+        # load the POS tags of response words
+        response_POS_map = self.load_POS_map(response)
+
+        # build the similarity vector and order vector between search words and total words
+        search_vectors = self.build_sim_vectors(search_words, total_words, self.search_POS_tags)
+
+        # build the similarity vector and order vector between response words and total words
+        self.word_measurer.load_synsets(response_words)
+        response_vectors = self.build_sim_vector(response_words, total_words, response_POS_tags)
+
+        # compute the semantic similarity with similarity vector and order vector
+        sim = self.compute_similarity(search_vectors, response_vectors)
+        return sim
+
+    # Measure each words pair of <total_word, search_word>, get the highest similarity
+    # and build the similarity vector and order vector based on similarity
+    def build_sim_vectors(self, search_words, total_words, POS_map):
+        sim_vector = []
+        order_vector = []
+        num_total_words = len(total_words)
+        num_search_words = len(search_words)
+        # iterate through the total_words to get the most similar word for each total_word
+        for i in num_total_words:
+            total_word = total_words[i]
+            total_word_weight = self.get_word_weight(total_word)
+
+            max_sim = 0
+            max_sim_index = 0
+            max_weight = 0
+            # iterate through the search_words to get the most similar search_word of a given total_word
+            for j in num_search_words:
+                POS_tag = None
+                if search_word in POS_map:
+                    POS_tag = POS_map[search_word]
+                search_word = search_words[j]
+                search_word_weight = self.get_word_weight(search_word)
+                # call the words measurer to get words' similarity between a pair of words
+                word_sim = self.word_measurer.measure_pair_words_sim(search_word, total_word, POS_tag)
+                if word_sim > max_sim:
+                    max_sim = word_sim
+                    max_sim_index = j+1
+                    max_weight = search_word_weight
+
+                if max_sim == 1:
+                    # if two words are identical, similarity is equal to 1, terminate loop
+                    break
+            sim_vector.append(max_sim * total_word_weight * max_weight)
+            order_vector.append(max_sim_index)
+         return [sim_vector, order_vector]
+
+    # Use the words frequency map to get the weight of a word
+    def get_word_weight(self, word):
+        word_frequency = self.words_frequency[word]
+        sim = 1 - (log(word_frequency + 1) / log(self.words_count + 1))
+        return sim
+
+    def list_dot_product(self, l1, l2):
+        return sum(i*j for i,j in zip(l1, l2))
+
+    def list_square_sum(self, l1):
+        return sum(i*i for i in l1)
+
+    def list_add(self, l1, l2):
+        return [i+j for i, j in zip(l1, l2)]
+
+    def list_sub(self, l1, l2):
+        return [i-j for i, j in zip(l1, l2)]
+
+    # Use the formula provided in the referenced paper to compute similarity of two phrases
+    def compute_similarity(self, search_vectors, response_vectors):
+        BETA = NLPPhraseSimMeasure.BETA
+        sim = BETA * (self.list_dot_product(search_vectors[0], response_vectors[0]) / self.list_square_sum(search_vectors[0]) * self.list_square_sum(response_vectors[0]) 
+              +
+              (1 - BETA) * (self.list_square_sum(self.list_sub(search_vectors[1], response_vectors[1])) / self.list_square_sum(self.list_add(search_vectors[1], response_vectors[1])))
+        return sim
--- relevant_resp_generator.py
+++ relevant_resp_generator.py
@@ -1,5 +1,6 @@
 from track import Track
-from phrase_sim_measurer import PhraseSimMeasurer
+from nlp_phrase_sim_measurer import NLPPhraseSimMeasurer
+from leven_phrase_sim_measurer import LevenPhraseSimMeasurer
 from spotify_client import SpotifyClient
 
 # A class generates the relevance response from spotify given a search query
@@ -20,7 +21,7 @@ class RelevantRespGenerator:
         playlist = self.get_playlist(search_query)
 
         # for each response track, compute the semantic similarity between search_query and response track name
-        self.measurer = PhraseSimMeasurer(self.brown_ic, search_query)
+        self.measurer = NLPPhraseSimMeasurer(self.brown_ic, search_query)
         for track in playlist:
             phrase_sim = self.measurer.measure_phrase_sim(track.name)
             track.set_similarity(phrase_sim)
@@ -34,8 +35,8 @@ class RelevantRespGenerator:
     def get_playlist(self, search_query):
         return self.client.get_playlist(search_query) 
 
-#spotify_client = SpotifyClient()
-#generator = RelevantRespGenerator(None, spotify_client)
-#small_playlist = generator.generate_response("happy friday")
-#for track in small_playlist:
-#    print track.name + ":" + track.album + ":" + str(track.similarity)
+spotify_client = SpotifyClient()
+generator = RelevantRespGenerator(None, spotify_client)
+small_playlist = generator.generate_response("happy friday")
+for track in small_playlist:
+    print track.name + ":" + track.album + ":" + str(track.similarity)
